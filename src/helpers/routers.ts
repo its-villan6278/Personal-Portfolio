7c6df0b006b3a17c704f1602098a93b600a25c3b
Aniket kr. Sinha <villanbabu30@gmail.com>
2023-06-24 21:10:38 +0530

    Update routers.ts

--- src/helpers/routers.ts
+++ src/helpers/routers.ts
@@ -1,9 +1,9 @@
 const Router = {
 ExternalRouter : {
-    Discord: 'https://discord.gg/VVgMYcySVZ',
-    Github: 'https://github.com/vishal889',
-    Instgram: 'https://www.instagram.com/vishal.099_/',
-    Twitter: 'https://twitter.com/Vishal_899',
+    Discord: 'https://discord.gg/qgop',
+    Github: 'https://github.com/its-villan6278',
+    Instgram: 'https://www.instagram.com/its_aniket_6278/',
+    Twitter: 'https://twitter.com/aniket9b',
 
 }
 }
